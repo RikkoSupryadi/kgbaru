f14016041f496169a4a3c5a283cd5c1df73eee17
RikkoSupryadi <madbaby69@gmail.com>
2018-09-05 09:31:39 +0700

    Lappup Filter Done
    READY TO LIVE BOII

--- template/lappub.php
+++ template/lappub.php
@@ -13,10 +13,14 @@ include('component/com-lappub.php');
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase">Laporan</h2>
           </div>
+          <div class="col-lg-4">
+            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Cari Laporan *" class="form-control">
+          </div>
         </div>
+        <br>
         <div class="row">
           <div class="col-sm-12">
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover" id="myTable">
         <tbody>
           <?php foreach($lappub as $lappub) { ?>
           <tr>
@@ -31,19 +35,5 @@ include('component/com-lappub.php');
         </tbody>
       </table>
     </div>
-    <div class="col-sm-12">
-            <form id="contactForm" name="sentMessage" novalidate="novalidate">
-              <div class="row">
-                <div class="col-md-4">
-                  <div class="form-group">
-                    <input class="form-control" id="name" type="text" placeholder="Cari Laporan *" required="required" data-validation-required-message="Form Kosong">
-                    <p class="help-block text-danger"></p>
-                  <button id="searchbtn" class="btn btn-primary btn-xl text-uppercase" type="submit">SEARCH</button>
-                </div>
-                <div class="clearfix"></div>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+  </div>
 </div>
